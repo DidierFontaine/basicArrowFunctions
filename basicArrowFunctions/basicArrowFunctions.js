dfdd580d7ff18072fd3731ab0d51475320191624
Didier Fontaine <dieds@live.nl>
2020-11-23 14:01:08 +0100

    map gemaakt met js file

---
+++ basicArrowFunctions/basicArrowFunctions.js
@@ -0,0 +1,19 @@
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions()
+
+  const fivePlusSeven = () => 5 + 7 
+  console.log (fivePlusSeven())
+  fivePlusSeven();
+
+  const myFunction = (a,b) => a + b
+  console.log (myFunction(1,2))
+
+  const addFive = a => a + 5
+  console.log (addFive(1))
+
+  const createObject = () => ({greeting: "hoi"})
+  console.log (createObject())
+  
